eb8dbc704cad574cf33780a4fb483148a4bb0ac7
WarriorKiller1098 <gotimenowinc123@gmail.com>
2025-09-28 17:44:48 +0530

    Create server.js

---
+++ app/server.js
@@ -0,0 +1,19 @@
+const http = require("http")
+
+http.createServer(function(req, res) {
+  const urlpath = url.parse(req.url, true)
+  const parsedpath = urlpath.path
+  const query = urlpath.query
+  const path = parsedpath.split("?")[0]
+  if (path.charAt(path.length - 1) == "?") {
+      query = parsedpath.split("?")[1].replace("?", "")
+  }
+  console.log("Method requested: " + req.method + ", with endpoint " + path);
+  if (path == "/") {
+    res.write("A test at render");
+    res.end();
+  } else if (path == "/status") {
+    res.write("good and healthy :)");
+    res.end();
+  }
+})
