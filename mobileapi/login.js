365d22759940849da5184292d3d087ad7a791362
WarriorKiller1098 <gotimenowinc123@gmail.com>
2025-10-10 15:07:38 +0530

    add login code
    no way

--- mobileapi/login.js
+++ mobileapi/login.js
@@ -1,1 +1,12 @@
-
+return {
+  "Status": "OK",
+  "UserInfo": {
+    "UserName": "testuser",
+    "UserPassword": "hello",
+    "UserID": 1,
+    "RobuxBalance": 150,
+    "TicketsBalance": 12000,
+    "IsAnyBuildersClubMember": true,
+    "ThumbnailUrl": "https://14blox.strangled.net/getUserAvatarImage?userId=1"
+  }
+}
