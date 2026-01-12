01361ea2c5dd9f390784e86c3fa78cab89a0ba88
WarriorKiller1098 <gotimenowinc123@gmail.com>
2026-01-12 21:06:24 +0530

    bugfix #24!

--- app.js
+++ app.js
@@ -16,13 +16,11 @@ const client = new MongoClient(uri, {
   }
 });
 async function run() {
-  try {
     // Connect the client to the server    (optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    }
 }
 process.on("SIGINT", async function() {
     await client.close()
