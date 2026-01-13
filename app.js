990425bcc499eb9190291088e63afbc82bfc913a
WarriorKiller1098 <gotimenowinc123@gmail.com>
2026-01-13 14:20:12 +0530

    bugfix #25!
    Finally, finally DB should be working

--- app.js
+++ app.js
@@ -16,11 +16,17 @@ const client = new MongoClient(uri, {
   }
 });
 async function run() {
+  try {
     // Connect the client to the server    (optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (err) {
+        console.error("Failed to connect to MongoDB", err);
+        process.exit(1);
+    }
+  }
 }
 process.on("SIGINT", async function() {
     await client.close()
@@ -78,7 +84,7 @@ http.createServer(async function(req, res) {
     }
     if (req.method == "POST") {
         if (path == "/mobileapi/login") {
-          let userDataM = GetUserData("WBtest");
+          let userDataM = await GetUserData("WBtest");
           res.write(JSON.stringify(userDataM));
         }
     }
